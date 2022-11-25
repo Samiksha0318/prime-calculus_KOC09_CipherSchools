ee851ae50a2e852aa1bbb60a26dd23a7b48ee840
Samiksha <114793981+Samiksha0318@users.noreply.github.com>
2022-11-25 19:31:01 +0530

    Add files via upload

---
+++ prime-calculus_KOC09_CipherSchools.py
@@ -0,0 +1,25 @@
+a=int(input("Enter lower range: "))
+b=int(input("Enter upper range: "))
+prime=0
+comp=0
+print(f"RANGE is({a},{b})")
+print("Then the status of each number in range is:")
+while a<=b:
+    flag=False
+    for i in range(2,a):
+        if a%i==0 :
+            flag=True
+            break
+    if a<=1:
+        print(a,"is composite or not prime")
+        comp+=1
+        a+=1
+        continue
+    elif flag:
+        print(a,"is composite or not prime")
+        comp+=1
+    else:
+        print(a,"is prime number")
+        prime+=1
+    a+=1
+print("COUNT:",prime,"prime and",comp,"composite numbers in the range.")
